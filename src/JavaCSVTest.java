cd56476717285d57b90ef62172c4e8db5e549ed4
Henry Scheible <henry.scheible@gmail.com>
2018-10-30 17:38:36 -0500

    finish tester file

--- src/JavaCSVTest.java
+++ src/JavaCSVTest.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
 
 public class JavaCSVTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int[][] Matrix = {{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
-		JavaCSV myjavacsv = new JavaCSV(Matrix);
+		JavaCSV myjavacsv = new JavaCSV(Matrix, "H:/test.csv");
+		myjavacsv.save();
+		
 	}
 
 }
