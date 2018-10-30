b9c9175c5a1a396fdd0342cf351a014e2b2c251e
Henry Scheible <ScheHenr0091@hopkins.hopkinsschools.org>
2018-10-30 17:09:54 -0500

    Write main.java and add constructor and format() method

--- main.java
+++ main.java
@@ -1,1 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class JavaCSV {
+	
+	private int[][] Data;
+	private String[] stringData;
+	private String stringDataFinal;
+	
+	public JavaCSV(int[][] data) {
+		this.Data = data;
+	}
+	
+	private format() {
+		for (int i = 0; i < data.length(); i++) {
+		   	stringData[i] = Data[i].toString();
+		   	stringData[i] = stringData[i]getClass().substring(1,stringData[i].length());
+		}
+		
+		for (int i = 0; i < stringData.length(); i++) {
+			stringDataFinal+=stringData[i];
+			stringDataFinal+="\n";
+		}
+	}
+}
+
 
