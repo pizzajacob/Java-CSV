b487597e6ee953e2c632634c614f883beffaabbd
pizzajess <40721768+pizzajess@users.noreply.github.com>
2018-11-03 09:10:52 -0500

    Make it kind of work sorta

--- src/JavaCSV.java
+++ src/JavaCSV.java
@@ -1,46 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author BernJaco2427
+ */
 import java.util.*;
 import java.io.*;
 
 public class JavaCSV {
-	
-	private int[][] Data;
-	private String[] stringData;
-	private String stringDataFinal;
-	private String fileName = "test.csv";
-	
-	public JavaCSV(int[][] data, String fileName) {
-		this.Data = data;
-		this.fileName = fileName;
-	}
-	
-	public JavaCSV(int[][] data) {
-		this.Data = data;
-	}
-	
-	
-	public void save() throws IOException {
-		for (int i = 0; i < Data.length; i++) {
-		   	stringData[i] = Arrays.toString(Data[i]);
-		   	stringData[i] = stringData[i].substring(1,stringData[i].length());
-		}
-		
-		for (int i = 0; i < stringData.length; i++) {
-			stringDataFinal+=stringData[i];
-			stringDataFinal+="\n";
 
-		   	stringData[i] = stringData[i].substring(1, stringData[i].length());
-		}
-		
-		for (int i = 0; i < stringData.length; i++) {
-			stringDataFinal += stringData[i];
-			stringDataFinal += "\n";
-		}
-		
-		FileWriter document = new FileWriter(fileName);
-		BufferedWriter bufDocument = new BufferedWriter(document);
-		bufDocument.write(stringDataFinal);
-		bufDocument.close();
-	}
+    private int[][] Data;
+    private String[] stringData;
+    private String stringDataFinal;
+    private String fileName = "test.csv";
+
+    public JavaCSV(int[][] data, String fileName) {
+        this.Data = data;
+        this.fileName = fileName;
+    }
+
+    public JavaCSV(int[][] data) {
+        this.Data = data;
+    }
+
+    public void save() throws IOException {
+        stringData = new String[Data.length];
+        for (int i = 0; i < Data.length; i++) {
+            stringData[i] = Arrays.toString(Data[i]);
+        }
+        
+        for (int i = 0; i < stringData.length; i++) {
+            
+            stringDataFinal += stringData[i];
+            stringDataFinal += "\n";
+        }
+        System.out.println(stringDataFinal);
+        stringDataFinal = stringDataFinal.substring(4, stringDataFinal.length());
+        System.out.println(stringDataFinal);
+        FileWriter document = new FileWriter(fileName);
+        BufferedWriter bufDocument = new BufferedWriter(document);
+        bufDocument.write(stringDataFinal);
+        bufDocument.close();
+    }
 }
-
-
