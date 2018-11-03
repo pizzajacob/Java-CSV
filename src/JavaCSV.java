beeb95c2a1ae2e46d862f73b3a3c1a316866ff31
pizzajess <40721768+pizzajess@users.noreply.github.com>
2018-11-03 09:36:11 -0500

    Completely working!

--- src/JavaCSV.java
+++ src/JavaCSV.java
@@ -1,23 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication1;
 
 /**
  *
  * @author BernJaco2427
  */
-import java.util.*;
 import java.io.*;
 
 public class JavaCSV {
 
     private int[][] Data;
     private String[] stringData;
-    private String stringDataFinal;
-    private String fileName = "test.csv";
+    private String fileName = "H:/test.csv";
 
     public JavaCSV(int[][] data, String fileName) {
         this.Data = data;
@@ -31,20 +24,24 @@ public class JavaCSV {
     public void save() throws IOException {
         stringData = new String[Data.length];
         for (int i = 0; i < Data.length; i++) {
-            stringData[i] = Arrays.toString(Data[i]);
+            stringData[i] = arrayToString(Data[i]);
         }
         
-        for (int i = 0; i < stringData.length; i++) {
-            
-            stringDataFinal += stringData[i];
-            stringDataFinal += "\n";
-        }
-        System.out.println(stringDataFinal);
-        stringDataFinal = stringDataFinal.substring(4, stringDataFinal.length());
-        System.out.println(stringDataFinal);
         FileWriter document = new FileWriter(fileName);
         BufferedWriter bufDocument = new BufferedWriter(document);
-        bufDocument.write(stringDataFinal);
+        
+        for (String line : stringData) {
+            bufDocument.write(line);
+        }
         bufDocument.close();
+    }
+    
+    public String arrayToString(int[] data) {
+        StringBuilder builder = new StringBuilder();
+        for (int value : data) {
+            builder.append(String.format("\"%s\",", value));
+        }
+        builder.append(System.lineSeparator());
+        return builder.toString();
     }
 }
